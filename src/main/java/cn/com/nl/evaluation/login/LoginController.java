64697e7595f070330a5095af324d6582257680d1
lj9108 <lj123456>
2015-08-03 23:16:11 +0800

    提交界面资源和界面控制

--- src/main/java/cn/com/nl/evaluation/login/LoginController.java
+++ src/main/java/cn/com/nl/evaluation/login/LoginController.java
@@ -1,23 +1,44 @@
 package cn.com.nl.evaluation.login;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import cn.com.nl.framework.base.BasicController;
+
 @Scope("prototype")
 @Controller
-public class LoginController {
+public class LoginController extends BasicController {
 
-	private Logger log = LoggerFactory.getLogger(LoginController.class);
-
+	/**
+	 *
+	 * 根据URL请求跳转到login。jsp画面
+	 *
+	 * @param model
+	 * @return
+	 */
 	@RequestMapping(value = "/login")
 	public ModelAndView doLogin(ModelMap model) {
+		return new ModelAndView("loginview", model);
+	}
 
-		model.addAttribute("test", "刘婧");
+	/**
+	 *
+	 * 执行login界面点击“登录”按钮后的响应方法
+	 *
+	 * @param model
+	 * @return
+	 */
+	@RequestMapping(value = "/loginProcess")
+	public ModelAndView doLoginProcess(ModelMap model) {
+
+		System.out.println("@@@@@@ 用户名 =" + getScreenParameterMap().get("user"));
+		System.out.println("@@@@@@ 密码  =" + getScreenParameterMap().get("pwd"));
+
+		model.addAttribute("returnValue", "123啊啊啊");
 
 		return new ModelAndView("loginview", model);
 	}
