f582ef6f96d54733e04efb24cfa02c25fad63035
lisiqi19891007jerry <819917495@qq.com>
2015-08-18 22:58:21 +0800

    增加生成数据库组件值的工具类

---
+++ src/main/java/cn/com/nl/framework/tools/SequenceUtil.java
@@ -0,0 +1,22 @@
+package cn.com.nl.framework.tools;
+
+import java.util.UUID;
+
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ * @Title SequenceUtil.java
+ * @Package cn.com.nl.framework.tools
+ * @Description 取得UUID工具类
+ * @Author Li Siqi
+ * @Date 2015年8月18日 下午10:49:44
+ * @Version V1.0
+ */
+public class SequenceUtil {
+
+	private static final String FILTER_STR = "-";
+
+	public static String getSequenceStr() {
+		return StringUtils.substringAfterLast(UUID.randomUUID().toString(), FILTER_STR);
+	}
+}
