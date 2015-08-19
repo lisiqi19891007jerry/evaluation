146e3783db6546e25eb918ffee32388a23539783
lisiqi19891007jerry <819917495@qq.com>
2015-08-19 22:43:59 +0800

    增加对AttributeValueModel进行排序的工具类

---
+++ src/main/java/cn/com/nl/framework/tools/AttributeValueModelComparator.java
@@ -0,0 +1,22 @@
+package cn.com.nl.framework.tools;
+
+import java.util.Comparator;
+
+import cn.com.nl.framework.model.AttributeValueModel;
+
+/**
+ * @Title AttributeValueModelComparator.java
+ * @Package cn.com.nl.framework.tools
+ * @Description 
+ * @Author Li Siqi
+ * @Date 2015年8月19日 下午7:41:43
+ * @Version V1.0
+ */
+public class AttributeValueModelComparator implements Comparator<AttributeValueModel> {
+
+	@Override
+	public int compare(AttributeValueModel valueModel1
+					  ,AttributeValueModel valueModel2) {
+		return Integer.compare(valueModel1.getAttributeRanking(), valueModel2.getAttributeRanking());
+	}
+}
---
+++ src/main/java/cn/com/nl/framework/tools/ListSortUtil.java
@@ -0,0 +1,24 @@
+package cn.com.nl.framework.tools;
+
+import java.util.Collections;
+import java.util.List;
+
+import cn.com.nl.framework.model.AttributeValueModel;
+
+/**
+ * @Title ListSortUtil.java
+ * @Package cn.com.nl.framework.tools
+ * @Description 
+ * @Author Li Siqi
+ * @Date 2015年8月19日 下午7:35:58
+ * @Version V1.0
+ */
+public class ListSortUtil {
+
+	public static List<AttributeValueModel> sortAttributeValueList(List<AttributeValueModel> valueModelList) {
+
+		Collections.sort(valueModelList, new AttributeValueModelComparator());
+
+		return valueModelList;
+	}
+}
