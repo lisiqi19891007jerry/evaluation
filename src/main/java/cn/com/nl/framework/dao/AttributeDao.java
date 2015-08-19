6512059f34d3c7e2ed3a68a482153d8b804655bc
lisiqi19891007jerry <819917495@qq.com>
2015-08-19 23:50:54 +0800

    添加查询c_attribute表数据的方法

---
+++ src/main/java/cn/com/nl/framework/dao/AttributeDao.java
@@ -0,0 +1,9 @@
+package cn.com.nl.framework.dao;
+
+import java.util.List;
+import java.util.Map;
+
+public interface AttributeDao {
+
+	public List<Map<String, Object>> doSelectAttribute();
+}
---
+++ src/main/java/cn/com/nl/framework/dao/impl/AttributeDaoImpl.java
@@ -0,0 +1,31 @@
+package cn.com.nl.framework.dao.impl;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import cn.com.nl.framework.dao.AttributeDao;
+
+@Repository
+public class AttributeDaoImpl implements AttributeDao {
+
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+
+	public List<Map<String, Object>> doSelectAttribute() {
+
+		StringBuffer sql = new StringBuffer();
+
+		sql.append(" SELECT attribute_index_name ");
+		sql.append("       ,attribute_name ");
+		sql.append("       ,attribute_value ");
+		sql.append("       ,attribute_ranking ");
+		sql.append("   FROM c_attribute ");
+		sql.append("  ORDER BY attribute_index_name ");
+
+		return jdbcTemplate.queryForList(sql.toString());
+	}
+}
